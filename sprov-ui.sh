f863232703cabe631e87914e3681bc58807f8542
sprov <47310637+sprov065@users.noreply.github.com>
2019-09-18 22:53:04 +0800

    Create sprov-ui.sh

---
+++ sprov-ui.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
+echo -e "sprov-ui 面板${red}已停止维护${plain}，请使用新面板：${green}v2-ui${plain}"
+echo -e ""
+echo -e "Github 地址：${green}https://github.com/sprov065/v2-ui${plain}"
+echo -e ""
+echo -e "博客教程：${green}https://blog.sprov.xyz/2019/08/03/v2-ui/${plain}"
+echo -e ""
+echo -e "一键安装命令：${green}bash <(curl -Ls https://blog.sprov.xyz/v2-ui.sh)${plain}"
+echo -e ""
